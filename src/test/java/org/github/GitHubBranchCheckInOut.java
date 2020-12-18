b3dfd4b886fa5fa11500b6d3c1a054765b1ddeb3
githubarunkumarg <arunkumarg.gmail.com>
2020-12-18 17:09:50 +0530

    Team User One Code Updates

--- src/test/java/org/github/GitHubBranchCheckInOut.java
+++ src/test/java/org/github/GitHubBranchCheckInOut.java
@@ -10,15 +10,12 @@ public class GitHubBranchCheckInOut {
 		System.out.println("Team User Two Update 1");
 		System.out.println("Team User Two Update 2");
 		System.out.println("Team User Two Update 3");
-		System.out.println("Team User One ");
-		System.out.println("Team User One  Checkout Update");
+		
 	}
 	@Test
 	public void teamUserTwotc2() {
 		System.out.println("Team User Two");
-		System.out.println("Team User One Update 1");
-		System.out.println("Team User One Update 2");
-	}
+			}
 	@Test
 	public void teamUserThreetc3() {
 		System.out.println("Team User Three");
