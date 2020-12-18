c36795ec62c7fa68f4c5fe6e4f7fec117b60f3d5
githubarunkumarg <arunkumarg.gmail.com>
2020-12-18 16:29:24 +0530

    Team User One Code Updates

--- src/test/java/org/github/GitHubBranchCheckInOut.java
+++ src/test/java/org/github/GitHubBranchCheckInOut.java
@@ -5,7 +5,8 @@ import org.testng.annotations.Test;
 public class GitHubBranchCheckInOut {
 	@Test
 	public void teamUserOnetc1() {
-		System.out.println("Team User One");
+		System.out.println("Team User One ");
+		System.out.println("Team User One  Checkout Update");
 	}
 	@Test
 	public void teamUserTwotc2() {
