f7428d7c511bc061d8613c047e15a9728ec66e2e
githubarunkumarg <arunkumarg.gmail.com>
2020-12-18 12:55:16 +0530

    First Commit

---
+++ src/test/java/org/github/GitHubBranchCheckInOut.java
@@ -0,0 +1,19 @@
+package org.github;
+
+import org.testng.annotations.Test;
+
+public class GitHubBranchCheckInOut {
+	@Test
+	public void teamUserOnetc1() {
+		System.out.println("Team User One");
+	}
+	@Test
+	public void teamUserTwotc2() {
+		System.out.println("Team User Two");
+	}
+	@Test
+	public void teamUserThreetc3() {
+		System.out.println("Team User Three");
+	}
+
+}
